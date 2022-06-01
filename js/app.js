da73c4d4a16bd98e046979323c4af40086083842
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-01 13:32:01 -0400

    create varibles board, turn, and winner to track state of the game

--- js/app.js
+++ js/app.js
@@ -3,9 +3,13 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
-
+//1. Define the required varibles used to track the state of the game
+//1a. varible named board to represent the state of the squares on the board.
+let board
+//1b. varible to track whose turn it is.
+let turn
+//1c. varible to represent if anyone has won yet, or if a tie has occured.
+let winner
 /*------------------------ Cached Element References ------------------------*/
 
 
