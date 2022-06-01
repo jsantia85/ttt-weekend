dbc5896b77e249e921d8e748fd4c118c85d5a83c
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-01 14:05:19 -0400

    creates cached element reference for elements representing board

--- js/app.js
+++ js/app.js
@@ -5,13 +5,16 @@
 /*---------------------------- Variables (state) ----------------------------*/
 //1. Define the required varibles used to track the state of the game
 //1a. varible named board to represent the state of the squares on the board.
-let board
+let board;
 //1b. varible to track whose turn it is.
-let turn
+let turn;
 //1c. varible to represent if anyone has won yet, or if a tie has occured.
-let winner
+let winner;
 /*------------------------ Cached Element References ------------------------*/
-
+//2. Store cached element references.
+//2a. Constant used to store the nine elements representing the squares on the page.
+const squareEls = document.querySelectorAll("div.sqEl0, div.sqEl1, div.sqEl2, div.sqEl3, div.sqEl4, div.sqEl5, div.sqEl6, div.sqEl7, div.sqEl8")
+//2b.
 
 
 /*----------------------------- Event Listeners -----------------------------*/
