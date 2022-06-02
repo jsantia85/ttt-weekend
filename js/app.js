3290b99d8939b6c0cb641f597db1fcf4c40b7041
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-02 18:29:36 -0400

    creates code to add X or O when square is clicked

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@ const winningCombos = [
   [[0, 1, 2], [0, 4, 8], [0, 3, 6], [3, 4, 5], [6, 7, 8], [1, 4, 7], [2, 5, 8], [2, 4, 6]]
 ];
 
-
 /*---------------------------- Variables (state) ----------------------------*/
 //1. Define the required varibles used to track the state of the game
 //1a. varible named board to represent the state of the squares on the board.
@@ -20,17 +19,11 @@ let winner;
 const squareEls = document.querySelectorAll("div.sq")
 //2b. store the element that displays the game's status on the page.
 const messageEl = document.querySelector("#message")
-//6b. Will need to create a new constant for the gameBoard
-const gameBoard = document.querySelector("section")
-// console.log(gameBoard)
 /*----------------------------- Event Listeners -----------------------------*/
 //6b. Add event listener to game board. On the 'click' event, it should call the candle click function. First, create gameboard and set it to the HTML element that has all the squares on the board.
-
-gameBoard.addEventListener("click", function(evt) {
-  handleClick()
-})
-
-
+squareEls.forEach(square=> {
+  square.addEventListener('click', handleClick)
+}) 
 /*-------------------------------- Functions --------------------------------*/
 //3. Upon loading, the game state should be initialized, and a funciton should be called to render this game state
 //3a. Create init funciton
@@ -40,7 +33,7 @@ init()
 function init () {
   //console.log('sanity check')
   //3c. Set board varible to an arrary containing nine nulls to represent empty squares
-  board = [1, -1, null, null, null, null, null, null, null]
+  board = [null, null, null, null, null, null, null, null, null]
   // console.log(board)
   //3d. Set the turn to 1 which sill represent player X
   turn = 1
@@ -88,6 +81,14 @@ function render () {
 //6. Handle a player clicking a square with a handleClick function
 //6a. Create a function called handleClick, it will have an event parameter
 
-function handleClick () {
-  console.log('hello')
+function handleClick(evt) {
+  // console.log('called handleClick')
+  // console.log(evt)
+  // console.log(evt.target)
+  // console.log(evt.target.id)
+  // console.log(evt.target.id.substring(2))
+  let sqIdx = evt.target.id.substring(2)
+  board[sqIdx] = turn
+  turn = turn * -1
+  render()
 }
